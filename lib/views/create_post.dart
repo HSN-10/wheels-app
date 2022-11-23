09325e93d384c71919154a5d56289e08f2606abc
Hassan <h-@outlook.in>
2022-11-23 10:21:49 +0300

    complate create post

--- lib/views/create_post.dart
+++ lib/views/create_post.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 const List<String> transmission = <String>['Automatic', 'Manual'];
 const List<String> condition = <String>['New', 'Used', 'Scrap'];
 const List<String> bodyType = <String>['Sedan', 'Pickup', 'Supercar'];
+const List<String> gasType = <String>['Diesel', 'Jayyid', 'Mumtaz', 'Super'];
 
 class CreatePost extends StatefulWidget{
   const CreatePost({Key? key}):super(key: key);
@@ -15,6 +16,7 @@ class _CreatePostState extends State<CreatePost> {
   String transmissionValue = transmission.first;
   String conditionValue = condition.first;
   String bodyTypeValue = bodyType.first;
+  String gasTypeValue = gasType.first;
   @override
   Widget build(BuildContext context) {
   final ButtonStyle flatButtonStyle = TextButton.styleFrom(
@@ -27,51 +29,65 @@ class _CreatePostState extends State<CreatePost> {
       appBar: AppBar(
         title: const Text("Create Post"),
         backgroundColor: Colors.green,
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.add))
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(12.0),
           child: Column(children: [
             const SizedBox(height: 15,),
-            const Text("Vehicle", style: TextStyle(fontSize: 20),),
+            const Text("Vehicle", style: TextStyle(fontSize: 18),),
             Container(
               color: Colors.white,
               child: Column(children:  [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Maker',
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tight(const Size(170, 50)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Maker',
+                        ),
+                      ),
+                    ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tight(const Size(170, 50)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Model',
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Model',
-                  ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Year',
-                  ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Colour',
-                  ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Doors',
-                  ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Engine Cylinders',
-                  ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tight(const Size(170, 50)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Year',
+                        ),
+                      ),
+                    ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tight(const Size(170, 50)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Kilometrage',
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 20,),
                 Row(
@@ -97,6 +113,57 @@ class _CreatePostState extends State<CreatePost> {
                 SizedBox(height: 20,),
                 Row(
                   children: const [
+                    Text("Condition"),
+                  ],
+                ),
+                DropdownButton(
+                  items: condition.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(), 
+                    value: conditionValue,
+                    onChanged: (String? value) {
+                      setState(() {
+                        conditionValue = value!;
+                      });
+                    },
+                    isExpanded: true,
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tight(const Size(170, 50)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Number Of Owners',
+                        ),
+                      ),
+                    ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints.tight(const Size(170, 50)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Number Of Accidents',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ]), 
+              ),
+              const SizedBox(height: 20,),
+              const Text("Body", style: TextStyle(fontSize: 18),),
+              Container(
+              color: Colors.white,
+              child: Column(children:  [
+                Row(
+                  children: const [
                     Text("Body Type"),
                   ],
                 ),
@@ -118,43 +185,50 @@ class _CreatePostState extends State<CreatePost> {
                 TextFormField(
                   decoration: const InputDecoration(
                     border: UnderlineInputBorder(),
-                    labelText: 'Kilometrage',
+                    labelText: 'Colour',
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Doors',
+                  ),
+                ),
+              ])
+              ),
+              const SizedBox(height: 20,),
+              const Text("Engine", style: TextStyle(fontSize: 18),),
+              Container(
+              color: Colors.white,
+              child: Column(children:  [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Engine Cylinders',
                   ),
                 ),
                 SizedBox(height: 20,),
                 Row(
                   children: const [
-                    Text("Condition"),
+                    Text("Body Type"),
                   ],
                 ),
                 DropdownButton(
-                  items: condition.map<DropdownMenuItem<String>>((String value) {
+                  items: gasType.map<DropdownMenuItem<String>>((String value) {
                       return DropdownMenuItem<String>(
                         value: value,
                         child: Text(value),
                       );
                     }).toList(), 
-                    value: conditionValue,
+                    value: gasTypeValue,
                     onChanged: (String? value) {
                       setState(() {
-                        conditionValue = value!;
+                        gasTypeValue = value!;
                       });
                     },
                     isExpanded: true,
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Number Of Owners',
-                  ),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Number Of Accidents',
-                  ),
-                ),
-              ]), 
+              ])
               )
           ],),
         ),
