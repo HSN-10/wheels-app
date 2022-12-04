03e727541d7f4c3c896c2912c50b005110e14302
Hassan <h-@outlook.in>
2022-12-04 23:03:04 +0300

    login api

--- lib/views/Auth/login.dart
+++ lib/views/Auth/login.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:wheels/views/Auth/register.dart';
-
+import 'package:http/http.dart' as http;
+import '../../services/remote_service.dart';
+import 'package:wheels/views/home_page.dart';
+import '../../services/globals.dart';
 class Login extends StatefulWidget{
   const Login({Key? key}):super(key: key);
 
@@ -9,6 +14,25 @@ class Login extends StatefulWidget{
 }
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
+
+  late String email;
+  late String password;
+
+login() async{
+  print(email);
+  http.Response response = await RemoteService().login(email, password);
+  Map responseMap = jsonDecode(response.body);
+  if(response.statusCode == 200){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (BuildContext context) => const HomePage())
+      );
+  }else{
+    errorSnackBar(context, responseMap.values.first[0].toString());
+  }
+}
+
+
   @override
   Widget build(BuildContext context) {
   final ButtonStyle flatButtonStyle = TextButton.styleFrom(
@@ -21,72 +45,69 @@ class _LoginState extends State<Login> {
         title: const Text("Wheels"),
         backgroundColor: Colors.green,
       ),
-      body: Form(
-        key: _formKey,
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(children: [
-              Image.network(
-                fit: BoxFit.cover,
-                "https://cdn2.iconfinder.com/data/icons/player-rounded-set/154/user-login-player-function-name-avatar-512.png",
-                width: 150,
-              ),
-              const SizedBox(height: 15,),
-              const Text("Login", style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500),),
-              const SizedBox(height: 20,),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'E-mail',
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(children: [
+                Image.asset(
+                  fit: BoxFit.cover,
+                  "assets/images/login.png",
+                  width: 150,
                 ),
-                validator: (value) {
-                  if(value == null || value.isEmpty)
-                    return 'The email field is required.';
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Password',
+                const SizedBox(height: 15,),
+                const Text("Login", style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500),),
+                const SizedBox(height: 20,),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'E-mail',
+                  ),
+                  onChanged: (value) => email = value,
+                  validator: (value) {
+                    if(value == null || value.isEmpty)
+                      return 'The email field is required.';
+                  },
                 ),
-                validator: (value) {
-                  if(value == null || value.isEmpty)
-                    return 'The password field is required.';
-                },
-              ),
-              const SizedBox(height: 20,),
-              TextButton(
-                style: flatButtonStyle,
-                onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
-                  );
-                }
-              },
-                child: const Text("Login", style: TextStyle(fontSize: 18, color: Colors.white),)
+                const SizedBox(height: 10,),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                  onChanged: (value) => password = value,
+                  validator: (value) {
+                    if(value == null || value.isEmpty)
+                      return 'The password field is required.';
+                  },
                 ),
-              const SizedBox(height: 10,),
-              TextButton(
-              style: TextButton.styleFrom(
-              minimumSize: const Size(500, 50),
-              backgroundColor: Colors.red,
-              padding: const EdgeInsets.all(5),
+                const SizedBox(height: 20,),
+                TextButton(
+                  style: flatButtonStyle,
+                  onPressed: login,
+                  child: const Text("Login", style: TextStyle(fontSize: 18, color: Colors.white),)
+                  ),
+                const SizedBox(height: 10,),
+                TextButton(
+                style: TextButton.styleFrom(
+                minimumSize: const Size(500, 50),
+                backgroundColor: Colors.red,
+                padding: const EdgeInsets.all(5),
+              ),
+                onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const Register();
+                },)
+                ),
+                child: const Text("Register", style: TextStyle(fontSize: 18, color: Colors.white),)
+                )
+              ],),
+            )
             ),
-              onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const Register();
-              },)
-              ),
-              child: const Text("Register", style: TextStyle(fontSize: 18, color: Colors.white),)
-              )
-            ],),
-          )
-          ),
+        ),
       )
     );
   }
